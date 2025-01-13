d5f3315b96a70f89054cb091ae81c544747be134
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-01-13 15:33:35 +0530

    feat: add file/folder edit feature.

--- src/components/FolderView/FolderView.jsx
+++ src/components/FolderView/FolderView.jsx
@@ -1,8 +1,8 @@
 // imports
-import { Link, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import styles from "./FolderView.module.css"
 import { format } from "date-fns";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // a simple function to convert the file size
 // to a human readable format for display purposes
@@ -24,26 +24,30 @@ const getFileSize = (bytes) => {
 // a folder/file view component
 export default function FolderView({folders, files, setFolders, setFiles,  selectedFile, setSelectedFile}){
 
+    const navigate = useNavigate();
     const {folderId, fileId} = useParams();
+    const [editName, setEditName] = useState("")
+    const [editable, setEditable] = useState(null)
 
+    // a simple useEffect to retrieve file information when a file is clicked
     useEffect(() => {
         async function getFileDetails(){
 
-                const response = await fetch(`http://localhost:3000/file-system/files/file/${folderId}/${fileId}`, {
-                    mode: 'cors',
-                    credentials: 'include'
-                })
-    
-                const data = await response.json();
-                console.log(data)
-                if (response.ok){
-                    setFolders([]);
-                    setFiles([]);
-                    setSelectedFile(data.file);
+            const response = await fetch(`http://localhost:3000/file-system/files/file/${folderId}/${fileId}`, {
+                mode: 'cors',
+                credentials: 'include'
+            })
 
-                } else{
-                    console.log('err');
-                }
+            const data = await response.json();
+            console.log(data)
+            if (response.ok){
+                setFolders([]);
+                setFiles([]);
+                setSelectedFile(data.file);
+
+            } else{
+                console.log('err');
+            }
 
         }
         if (fileId) getFileDetails();
@@ -94,6 +98,58 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
 
     }
 
+    // a simple async function to handle folder edits
+    const handleFolderEdit = async (event, folderId) => {
+        event.preventDefault();
+        const response = await fetch(`${import.meta.env.VITE_DEVELOPMENT_SERVER}/file-system/folders`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},  
+            mode: 'cors',   
+            body: JSON.stringify({folderName: editName, folderId}),
+            credentials: 'include'
+        })
+
+        if (response.ok){
+            const updatedFolders = folders.filter(folder => folder.name === editName)
+            setFolders(prev => ([...updatedFolders]));
+            setEditName("");
+            setEditable(false);
+        }
+        else{
+            console.log('err')
+        }
+    }
+
+    // a simple async function to handle file edits
+    const handleFileEdit = async (event, fileId) => {
+        event.preventDefault();
+        const response = await fetch(`${import.meta.env.VITE_DEVELOPMENT_SERVER}/file-system/files`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},  
+            mode: 'cors',   
+            body: JSON.stringify({fileName: editName, fileId}),
+            credentials: 'include'
+        })
+
+        if (response.ok){
+            const updatedFiles = files.filter(file => file.name === editName)
+            setFiles(prev => ([...updatedFiles]));
+            setEditName("");
+            setEditable(false);
+        }
+        else{
+            console.log('err')
+        }
+    }       
+
+    // a simple function to handle input change
+    // for renaming a file/folder
+    const handleRenameChange = (event) => {
+        setEditName(event.target.value)
+    }    
+
+
+
     return (
         <>
             <div className={styles["folder-details-header"]}>
@@ -127,8 +183,8 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
                                 <p>{format(selectedFile.createdAt, 'dd/MM/yyyy')}</p>
 
                                 <div className={styles["file-right-options"]}>
-                                    <img alt="file delete icon" src="/public/folder_delete_icon.svg"></img>
-                                    <img alt="file edit icon" src="/public/folder_edit_icon.svg"></img>
+                                    <img title="Confirm Rename" alt="file delete icon" src="/public/folder_delete_icon.svg"></img>
+                                    <img title="Cancel Rename" alt="file edit icon" src="/public/folder_edit_icon.svg"></img>
                                 </div>
 
                             </div>
@@ -146,12 +202,23 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
                 </div>
             )}
             {folders.length !== 0 && folders.map(folder => (
-                <Link key={folder.id} className={styles["folder-container"]} to={`/tree/${folderId || 'root'}/${folder.id}`}>
-                    <div className={styles["folder"]}>
+                <Link key={folder.id} className={styles["folder-container"]} onDoubleClick={() => navigate(`/tree/${folderId || 'root'}/${folder.id}`)}  >
+                    <div className={styles["folder"] }>
 
                         <div className={styles["folder-left"]}>
                             <img src="/public/folder_open_icon.svg" alt="folder icon"></img>
-                            <p>{folder.folderName}</p>
+                            {editable == folder.id ? 
+
+                            <div className={styles["edit-input-container"]}>
+                                <input value={editName} onChange={handleRenameChange} id={styles["edit-input"]} type="text" name="rename" placeholder="Click To Rename..." /> 
+
+                                <div className={styles["edit-options"]}>
+                                    <img id={styles["confirm-rename-btn"]} src="/public/tick_icon.svg" alt="rename confirm button" onClick={(e) => handleFolderEdit(e, folder.id)} />
+                                    <img id={styles["confirm-rename-btn"]} src="/public/close_icon.svg" alt="rename confirm button" />
+                                </div>    
+                                                     
+                            </div>
+                            : <p>{folder.folderName}</p>}
                         </div>
 
                         <div className={styles["folder-right"]}>
@@ -160,7 +227,7 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
 
                             <div className={styles["folder-right-options"]}>
                                 <img alt="folder delete icon" src="/public/folder_delete_icon.svg" title="Delete Folder" onClick={(e) => handleFolderDelete(e, folder.id)}></img>
-                                <img alt="folder edit icon" src="/public/folder_edit_icon.svg" title="Edit Folder"></img>
+                                <img alt="folder edit icon" src="/public/folder_edit_icon.svg" title="Edit Folder" onClick={(e) => setEditable(folder.id)}></img>
                             </div>
 
                         </div>
@@ -169,12 +236,23 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
             ))}
 
         {files.length !== 0 && files.map(file => (
-                <Link key={file.id} className={styles["file-container"]} to={`/tree/file/${file.folderId}/${file.id}`}>
+                <Link key={file.id} className={styles["file-container"]} onDoubleClick={() => navigate(`/tree/file/${file.folderId}/${file.id}`)}>
                     <div className={styles["file"]}>
 
                         <div className={styles["file-left"]}>
                             <img alt="file icon" src="/public/file_icon.svg"></img>
-                            <p>{file.fileName}</p>
+                            {editable == file.id ? 
+
+                            <div className={styles["edit-input-container"]}>
+                                <input value={editName} onChange={handleRenameChange} id={styles["edit-input"]} type="text" name="rename" placeholder="Click To Rename..." /> 
+
+                                <div className={styles["edit-options"]}>
+                                    <img id={styles["confirm-rename-btn"]} src="/public/tick_icon.svg" alt="rename confirm button" onClick={(e) => handleFileEdit(e, file.id)} />
+                                    <img id={styles["confirm-rename-btn"]} src="/public/close_icon.svg" alt="rename confirm button" />
+                                </div>    
+                                                     
+                            </div>
+                            : <p>{file.fileName}</p>}
                         </div>
 
                         <div className={styles["file-right"]}>
@@ -186,7 +264,7 @@ export default function FolderView({folders, files, setFolders, setFiles,  selec
 
                             <div className={styles["file-right-options"]}>
                                 <img alt="file delete icon" src="/public/folder_delete_icon.svg" onClick={(e) => handleFileDelete(e, file.id)}></img>
-                                <img alt="file edit icon" src="/public/folder_edit_icon.svg"></img>
+                                <img alt="file edit icon" src="/public/folder_edit_icon.svg" onClick={() => setEditable(file.id)}></img>
                             </div>
 
                         </div>
