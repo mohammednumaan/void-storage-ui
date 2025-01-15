d563ace793d951d55de2d091662d76394b5c4446
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-01-15 12:02:42 +0530

    feat: implemented mobile bottom menu bar.

--- src/components/MobileSideBar/MobileSideBar.jsx
+++ src/components/MobileSideBar/MobileSideBar.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
 import FileFolderModal from "../Modal/CreateFileFolderModal";
 import styles from "./MobileSideBar.module.css"
+import { Link } from "react-router-dom";
 // onClick={/* () => setFolderForm(true)*/}
 // onClick={() => setFileForm(true)}
 
 export default function MobileSideBar({setFolders, setFiles}){
+
     // modal states to render the appropriate forms (file and folder creationi forms)
     const [fileForm, setFileForm] = useState(false);
     const [folderForm, setFolderForm] = useState(false); 
+
     return (
 
         <>
             <div className={styles["mobile-sidebar-container"]}>
-                <div className={styles["mobile-file-options"]}>
-                    <button id={styles['add-folder-btn']} onClick={() => setFileForm(true)}>
+          
+                    <button id={styles['add-folder-btn']} onClick={() => setFolderForm(true)}>
                         <img alt='create new file icon' src='/public/new_folder_icon.svg' title='Create Folder' />
                     </button>
 
-                    <button id={styles['add-file-btn']} onClick={() => setFolderForm(true)}>
+                    <Link to={`/tree`}>
+                        <button id={styles['home-folder-btn']}>
+                            <img alt='home icon' src='/public/home_icon.svg' title='Root Folder' />
+                        </button>
+                    </Link>
+
+                    <button id={styles['add-file-btn']} onClick={() => setFileForm(true)}>
                         <img alt='create new file icon' src='/public/new_file_icon.svg' title='Upload File'/>
                     </button>
-                </div>
             </div>
 
             {fileForm && <FileFolderModal formConfigObject={{formType: "File"}} setFileForm={setFileForm} setFileFolders={setFiles} />}
             {folderForm && <FileFolderModal formConfigObject={{formType: "Folder"}} setFolderForm={setFolderForm} setFileFolders={setFolders} />}  
-        
         </>
     )
 }
