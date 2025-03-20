d96e436e435c57989f5fb82d805293df9a790418
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-03-20 12:46:21 +0530

    feat: added logout functionality.

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -1,9 +1,26 @@
 // imports
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styles from "./NavigationBar.module.css"
 
 // navigation bar component
 export default function NavigationBar(){
+
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        const response = await fetch(`${import.meta.env.VITE_DEVELOPMENT_SERVER}/users/logout`, {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include'
+        })
+
+        const data = await response.json();    
+        console.log(data)
+        if (response.ok){
+            navigate('/')
+        } else{
+            
+        }
+    }
     return (
         <>
             <div className={styles["navbar-container"]}>
@@ -14,7 +31,7 @@ export default function NavigationBar(){
 
                 <div className={styles["navbar-links"]}>
                 <Link to='/'><img src="/public/account_icon.svg" alt="account button icon" title="Account" /></Link>
-                <Link to='/'><img src="/public/logout_icon.svg" alt="logout button icon" title="Logout" /></Link>
+                <img onClick={handleLogout} src="/public/logout_icon.svg" alt="logout button icon" title="Logout" />
                 </div>
             </div>
         </>
