00f33a7c859fc36a05c97908af1aa16d5a40ce61
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-03-31 15:34:07 +0530

    build: add base path for vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      base: 'https://void-storage-frontend-bgr2qnilw-mohammednumaans-projects.vercel.app/',
+      plugins: [react()],
+    })
